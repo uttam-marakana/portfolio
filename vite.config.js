598a696ca65197fe75a0c11a552b4e35e45f317e
uttam-marakana <uttammarakana03@gmail.com>
2026-02-16 09:53:34 +0530

    Modified : : Code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), require("@tailwindcss/typography")],
+  plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000,
   },
